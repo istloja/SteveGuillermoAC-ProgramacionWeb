1b3d31fe78dc9cdf165072af34ab9f9313a965b7
SteveGuillermoAC <45379600+SteveGuillermoAC@users.noreply.github.com>
2019-12-12 14:21:32 -0500

    Sincronizacion Formulario agregado Agregado selectedonebutton y selectedmanybutton Filtro de paginas

--- SteveGuillermoAC-ProgramacionWeb/src/java/Beans/PrimerBean.java
+++ SteveGuillermoAC-ProgramacionWeb/src/java/Beans/PrimerBean.java
@@ -9,7 +9,10 @@ import java.io.Serializable;
 import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import org.primefaces.PrimeFaces;
 
 @Named(value = "primerBean")
 @ViewScoped
@@ -22,6 +25,24 @@ public class PrimerBean implements Serializable {
     private String nombre;
     private String opcion;
     private List <String> lista;
+    private String nombreUsuario ;
+    private String claveUsuario ;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getClaveUsuario() {
+        return claveUsuario;
+    }
+
+    public void setClaveUsuario(String claveUsuario) {
+        this.claveUsuario = claveUsuario;
+    }
 
     public String getOpcion() {
         System.out.println("Opcion"+opcion);
@@ -59,5 +80,20 @@ public class PrimerBean implements Serializable {
     
     public PrimerBean() {
     }
+    public String add() {
+        String url=null;
+        FacesMessage message;
+        if(nombreUsuario.equals("usuario001") && claveUsuario.equals("123456")){
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido","");
+            url="/pagina_cliente2.xhtml?faces-redirect=true";
+        }
+        else{
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Datos Incorrectos","");
+        }
+        FacesContext.getCurrentInstance().addMessage(null, message);
+      return url;  
+    }
+
     
 }
+
